466765f0991279f6515caea426bc8986d0c96cd1
cferdinandi <chris@gomakethings.com>
2016-05-10 22:59:07 -0400

    v10.23.0 Updated the line height for h1-h6 elements to 1.4

--- dist/js/detects.js
+++ dist/js/detects.js
@@ -1,5 +1,5 @@
 /*!
- * gomakethings v10.22.2: The WordPress theme for GoMakeThings.com
+ * gomakethings v10.23.0: The WordPress theme for GoMakeThings.com
  * (c) 2016 Chris Ferdinandi
  * MIT License
  * https://github.com/cferdinandi/gomakethings
