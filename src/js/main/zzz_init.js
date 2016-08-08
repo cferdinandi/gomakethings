e37ade2727cfa8b017277c6a76baf6714421644b
cferdinandi <chris@gomakethings.com>
2016-08-08 16:17:15 -0400

    v10.38.0 Added Smooth Scroll

--- src/js/main/zzz_init.js
+++ src/js/main/zzz_init.js
@@ -6,3 +6,9 @@ fluidvids.init({
 	selector: ['iframe', 'object'],
 	players: ['www.youtube.com', 'player.vimeo.com', 'www.slideshare.net', 'www.hulu.com']
 });
+
+if ( document.querySelector( 'a[href*="#"]' ) ) {
+	smoothScroll.init({
+		selector: 'a'
+	});
+}
