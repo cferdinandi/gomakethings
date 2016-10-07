3a13cc2a17db49e5d6ee610f8c7b5a187538ce60
cferdinandi <chris@gomakethings.com>
2016-10-07 15:02:02 -0400

    v10.56.0 Added VideoPress to FluidVids.js

--- src/js/main/zzz_init.js
+++ src/js/main/zzz_init.js
@@ -4,7 +4,7 @@
 
 fluidvids.init({
 	selector: ['iframe', 'object'],
-	players: ['www.youtube.com', 'player.vimeo.com', 'www.slideshare.net', 'www.hulu.com']
+	players: ['www.youtube.com', 'player.vimeo.com', 'www.slideshare.net', 'www.hulu.com', 'videopress.com/embed/']
 });
 
 if ( document.querySelector( 'a[href*="#"]' ) ) {
