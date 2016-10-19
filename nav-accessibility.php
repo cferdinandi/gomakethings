1e7818e4c3150674c1900be3d59f8283409824dd
cferdinandi <chris@gomakethings.com>
2016-10-19 16:44:48 -0400

    v10.57.0 New navigation

--- nav-accessibility.php
+++ nav-accessibility.php
@@ -12,15 +12,8 @@
 	<!-- Skip nav link for better a11y -->
 	<a class="screen-reader screen-reader-focusable" href="#main"><?php _e( 'Skip to main content', 'keel' ); ?></a>
 
-	<?php
-		// Comment out with PHP
-		if ( false ) :
-	?>
-
 	<!-- Skip to secondary nav -->
 	<a class="screen-reader screen-reader-focusable" href="#nav-secondary"><?php _e( 'Skip to secondary navigation', 'keel' ); ?></a>
-
-	<?php endif; ?>
 
 	<!-- a11y feedback -->
 	<a class="screen-reader screen-reader-focusable" href="mailto:<?php echo antispambot( get_option( 'admin_email' ) ); ?>?subject=Go%20Make%20Things:%20Accessibility%20Feedback"><?php _e( 'Accessibility Feedback', 'keel' ); ?></a>
