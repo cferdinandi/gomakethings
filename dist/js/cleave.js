200ac9b5a470174c2ecd553af6cd2ca0d784c7e8
cferdinandi <chris@gomakethings.com>
2017-06-21 13:47:50 -0400

    Added smooth scroll ignore

--- dist/js/cleave.js
+++ dist/js/cleave.js
@@ -1,5 +1,5 @@
 /*!
- * gomakethings v10.88.1: The WordPress theme for GoMakeThings.com
+ * gomakethings v10.88.2: The WordPress theme for GoMakeThings.com
  * (c) 2017 Chris Ferdinandi
  * MIT License
  * https://github.com/cferdinandi/gomakethings
